0c40bc0af984b7c261a3c5817b79047ea117d1c5
PeaStew <34198053+PeaStew@users.noreply.github.com>
2018-03-25 16:56:15 +0200

    Update CheckAndChallenge.sh

--- CheckAndChallenge.sh
+++ CheckAndChallenge.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
-homeServer=$(cat ~/zencash/secnodetracker/config/home | cut -b 3-);
-nodeId=$(cat ~/zencash/secnodetracker/config/nodeid);
-tAddress=$(zen-cli listaddresses | grep -v ',\|\[\|]' | tr -d '\"\ ');
-challenges=$(curl -s "https://securenodes$homeServer.zensystem.io/api/grid/$nodeId/crs")
-challengeResult=$(python -c "import sys, json; print json.load(sys.stdin)['rows'][0]['result']" $challenges)
-if [ $result == 'fail' ];
-then curl -s "https://securenodes$homeServer.zensystem.io/$tAddress/$nodeId/send"; fi
+wait=0
+fail=0
+> failures.txt
+while IFS='' read -r line || [[ -n "$line" ]]; do
+        fqdn=$(echo $line | awk '{printf$1}')
+        homeServer=$(echo $line | awk '{printf$2}')
+        nodeId=$(echo $line | awk '{printf$3}')
+        tAddr=$(echo $line | awk '{printf$4}')
+        challenges=$(curl -s "https://securenodes$homeServer.zensystem.io/api/grid/$nodeId/crs")
+        challengeRes=$(echo ${challenges} | jq -r '.rows[0].result')
+        if [[ "$challengeRes" == 'wait' ]]
+        then
+                echo "$fqdn : $challengeRes"
+                let wait++
+        fi
+        if [[ "$challengeRes" == 'fail' ]]
+        then
+                echo "$fqdn : $challengeRes"
+                if [ $fail -lt 3 ]
+                then
+                        echo "curl -s \"https://securenodes$homeServer.zensystem.io/$tAddr/$nodeId/send\"" >> failures.txt
+                fi
+                let fail++
+        fi
+
+done < nodeDetails.txt
+
+echo $wait
+echo $fail
+executed=$wait
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [ $executed -ge 4 ]
+        then
+                exit 1
+        fi
+        eval $line
+        let executed++
+done < failures.txt
