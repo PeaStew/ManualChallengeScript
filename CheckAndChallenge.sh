019846409fb05559ef8154e8cb7b0f4579efb0c5
PeaStew <34198053+PeaStew@users.noreply.github.com>
2018-03-25 18:57:59 +0200

    Update CheckAndChallenge.sh

--- CheckAndChallenge.sh
+++ CheckAndChallenge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 wait=0
 fail=0
-> /home/peastew/failures.txt
+> failures
+> other
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
         fqdn=$(echo $line | awk '{printf$1}')
         homeServer=$(echo $line | awk '{printf$2}')
@@ -12,11 +14,12 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
         if [[ "$challengeRes" == 'wait' ]]
         then
                 let wait++
-        fi
-        if [[ "$challengeRes" == 'fail' ]]
+        elif [[ "$challengeRes" == 'fail' ]]
         then
-                echo "curl -s \"https://securenodes$homeServer.zensystem.io/$tAddr/$nodeId/send\"" >> failures.txt
+                echo "curl -s \"https://securenodes$homeServer.zensystem.io/$tAddr/$nodeId/send\"" >> failures
                 let fail++
+        else
+                echo "$fqdn : $challengeRes" >> other
         fi
 
 done < /home/peastew/nodeDetails.txt
@@ -25,11 +28,11 @@ done < /home/peastew/nodeDetails.txt
 #echo $fail
 executed=$wait
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
+while fails='' read -r line || [[ -n "$line" ]]; do
         if [ $executed -ge 3 ]
         then
                 exit 1
         fi
         eval $line > /dev/null
         let executed++
-done < /home/peastew/failures.txt
+done < failures
